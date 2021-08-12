c532dbec035dcb8336837f2d91554741c15e04c2
th3dr34d3d1 <ajoers@gmail.com>
2021-08-12 17:07:04 -0400

    created search view, controller, route; created welcometest ->tabled display instead of basic landingpage

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,20 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+
+
+
+
+//controller handling search capability
+class searchController extends Controller
+{
+	public function search(Request$request)
+	{
+		return view('searchResult');
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('landingpage');
+    return view('welcometest');
 });
+
+//route handling search capability
+Route::get('/search', [searchController::class, 'search']);
